553e15204c0b0b24eb62e18b76d6d5b940d12b2d
Anton Golovanov <agolovanov256@gmail.com>
2024-07-24 17:32:28 +0300

    units - add method to ensure units of a value

--- pic_utils/units.py
+++ pic_utils/units.py
@@ -22,3 +22,28 @@ def strip_units(value, target_unit: pint.Unit | str):
         return value.m_as(target_unit)
     else:
         return value
+
+
+def ensure_units(value, units: pint.Unit | str):
+    """Takes the value that may or may not have units and returns the value with specified units.
+
+    Parameters
+    ----------
+    value : pint.Quantity | float
+        the value to be returned
+    units : pint.Unit | str
+        the desired unit
+
+    Returns
+    -------
+    pint.Quantity
+        value in the desired units
+    """
+
+    if isinstance(value, pint.Quantity):
+        return value.to(units)
+    else:
+        if not isinstance(units, pint.Unit):
+            ureg = pint.get_application_registry()
+            units = ureg[units]
+        return value * units
--- tests/test_units.py
+++ tests/test_units.py
@@ -1,6 +1,6 @@
 import pint
 
-from pic_utils.units import strip_units
+from pic_utils.units import strip_units, ensure_units
 import numpy as np
 
 
@@ -11,3 +11,14 @@ def test_strip_units():
     results = [strip_units(v, 'm') for v in values]
 
     np.testing.assert_allclose(results, 0.05)
+
+
+def test_ensure_units():
+    ureg = pint.get_application_registry()
+    values = [0.05 * ureg.m, 5.0, 5.0 * ureg.cm]
+
+    results = [ensure_units(v, 'cm').m_as('cm') for v in values]
+    np.testing.assert_allclose(results, 5.0)
+
+    results = [ensure_units(v, ureg.cm).m_as('cm') for v in values]
+    np.testing.assert_allclose(results, 5.0)
