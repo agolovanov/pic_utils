f1f11a8a1aabdffc380d894e7ae1621506dbf71d
Anton Golovanov <agolovanov256@gmail.com>
2025-04-22 18:54:39 +0300

    Linear wake currents and energies calculation

--- pic_utils/linear_wake.py
+++ pic_utils/linear_wake.py
@@ -65,6 +65,8 @@ def calculate_laser_linear_wake(
     z: np.ndarray,
     r: np.ndarray,
     *,
+    currents: bool = False,
+    energy: bool = False,
     plasma_density: 'PlasmaUnits | pint.Quantity | None' = None,
     unitless: bool = False,
 ) -> dict:
@@ -80,6 +82,10 @@ def calculate_laser_linear_wake(
         the longitudinal coordinate (along which the wake is calculated)
     r : np.ndarray
         the transverse coordinate
+    currents : bool, optional
+        if True, the wakefield currents are calculated, by default False
+    energy : bool, optional
+        if True, the wakefield energy is calculated, by default False
     plasma_density : PlasmaUnits | pint.Quantity
         a value representing the plasma density. Can be an instance of PlasmaUnits or a Quantity fully determining the density (density, frequency, wavelength), by default None
     unitless : bool, optional
@@ -93,6 +99,9 @@ def calculate_laser_linear_wake(
         - 'ez' (longitudinal electric field)
         - 'er' (transverse electric field)
         - 'drho' (electron charge density variation)
+        - 'jz' and 'jr' (if currents is True, the longitudinal and transverse current densities)
+        - 'w_tilde_em', 'w_tilde_e', 'w_tilde' (if energy is True, the electromagnetic and electric energy densities)
+        - 'Psi_em', 'Psi_e', 'Psi' (if energy is True, the electromagnetic and electric energy densities integrated over the transverse coordinate)
     """
     from scipy.integrate import cumulative_trapezoid
 
@@ -126,6 +135,21 @@ def calculate_laser_linear_wake(
     result['ez'] = np.real(base_integral)
     result['er'] = -np.imag(base_integral_dtrans)
     result['drho'] = -ponderomotive_potential + result['psi'] - np.imag(base_integral_dtrans_2)
+
+    if currents or energy:
+        jz = -ponderomotive_potential + result['psi']
+        jr = np.real(base_integral_dtrans)
+        if currents:
+            result['jz'] = jz
+            result['jr'] = jr
+
+    if energy:
+        result['w_tilde_em'] = 0.5 * (result['er'] ** 2 + result['ez'] ** 2)
+        result['w_tilde_e'] = ponderomotive_potential + 0.5 * (jz**2 + jr**2)
+        result['w_tilde'] = result['w_tilde_em'] + result['w_tilde_e']
+        result['Psi_em'] = np.pi * np.trapezoid(np.abs(r)[:, None] * result['w_tilde_em'], dx=dr, axis=0)
+        result['Psi_e'] = np.pi * np.trapezoid(np.abs(r)[:, None] * result['w_tilde_e'], dx=dr, axis=0)
+        result['Psi'] = np.pi * np.trapezoid(np.abs(r)[:, None] * result['w_tilde'], dx=dr, axis=0)
 
     if not unitless:
         result['psi'] = pu.convert_to_units(result['psi'], 'phi')
